45f74120a472b88981ec95048e7ef410befa1b5f
Danilo Krstonijevic <dkrstonijevic@yahoo.com>
2016-09-01 13:42:05 +0200

    rad rad i samo rad

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$(document).ready(function () {
 
+    //Podesavanje funkcije za brzinu automatske promene slike za prvi slajder na INDEX stranici
+    $('.carousel').carousel({
+        interval: 3000 //changes the speed
+    });
+
+
+    
+    
+});
